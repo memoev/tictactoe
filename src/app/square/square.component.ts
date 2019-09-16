5ff4ac0d93f3aec7a94aeddee80aec475f44c3b8
Guillermo Villalta <mexcelus@gmail.com>
2019-09-16 15:49:16 -0600

    input into component for X and O

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
   template: `
-    <p>
-      square works!
-    </p>
+    <button>{{ value }}</button>
   `,
   styles: []
 })
 export class SquareComponent {
+
+  @Input() value: 'X' | 'O';
 
 }
